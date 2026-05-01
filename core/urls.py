d7cc756e869baa1bd49f49f9348cb4c2c42874bd
MuhammedKeremDemirbent <keremdmrbnt03@gmail.com>
2026-05-01 23:39:46 +0300

    Core

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), 
     path('', include('mainpage.urls')),
     path('pages/', include('pages.urls')),
     path('accounts/', include('accounts.urls')),
